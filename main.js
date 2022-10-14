2ddce53bc9b1bcf2f56351be16c9726b3c460085
Yani <yanivlascogmail.com>
2022-10-14 10:18:44 +0200

    test git

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const question2 = document.querySelector(".secondQuestion");
 const openQuestion2 = document.querySelector(".answerA2");
 
 openQuestion2.addEventListener("click", () => {
-    question2.style.display = "grid";
+//     question1.style.transform = " translateY(-100px);"
     question1.style.display = "none";
+    question2.style.display = "grid";
+    
 
 
 });
